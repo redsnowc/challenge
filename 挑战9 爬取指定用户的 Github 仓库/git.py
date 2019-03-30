dbae714670436b753afa1210c64f0ea2bb8f879b
redsnowc <nuanyang.44@gmail.com>
2019-03-30 20:16:51 +0800

    Create git.py

---
+++ 挑战9 爬取指定用户的 Github 仓库/git.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GitSpider(scrapy.Spider):
+    name = 'git'
+    start_urls = ['https://github.com/shiyanlou?tab=repositories']
+
+    def parse(self, response):
+        for value in response.css('li.col-12'):
+            yield{
+                'name': value.css(
+                    'div.d-inline-block.mb-1 a::text'
+                ).extract_first().strip(),
+                'update_time': value.css(
+                    'relative-time::attr(datetime)').extract_first()
+            }
+
+        for url in response.xpath(
+                '//a[contains(@class, "btn-outline")]/@href').extract():
+            if 'after' in url:
+                yield scrapy.Request(url, callback=self.parse)
