f28c3249860be63764c09d6a88e7eda80688faef
redsnowc <nuanyang.44@gmail.com>
2019-03-31 00:10:58 +0800

    update

--- 挑战17 基于学习数据绘图/datafigure.py
+++ 挑战17 基于学习数据绘图/datafigure.py
@@ -3,11 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-user_info = pd.read_json("user_study.json")
-user_study_time = user_info[['user_id', 'minutes']].groupby('user_id').sum()
-plt.plot(user_study_time)
-plt.title('StudyData')
-plt.xticks(np.arange(0, 200001, 50000))
-plt.xlabel('User ID')
-plt.ylabel('Study Time')
-plt.show()
+def data_plot():
+    user_info = pd.read_json("user_study.json")
+    user_study_time = user_info[['user_id', 'minutes']].groupby('user_id').sum()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    user_study_time.plot(ax=ax)
+    ax.set_xlabel('User ID')
+    ax.set_ylabel('Study Time')
+    plt.show()
+
+    return ax
+
+if __name__ == '__main__':
+    data_plot()
