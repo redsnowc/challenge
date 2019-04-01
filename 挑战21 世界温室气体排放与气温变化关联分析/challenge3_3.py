8a0cf591a579e259fe5f9ac45f1604243b532099
redsnowc <nuanyang.44@gmail.com>
2019-04-01 20:23:02 +0800

    更新挑战21三种绘图

--- 挑战21 世界温室气体排放与气温变化关联分析/challenge3_3.py
+++ 挑战21 世界温室气体排放与气温变化关联分析/challenge3_3.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def climate_plot():
@@ -19,30 +20,37 @@ def climate_plot():
         pd.to_datetime(data2['Date']))
     temp1 = temp['1990': '2010'].resample('A').mean().set_index(ghg.index)
     temp1 = (temp1 - temp1.min()) / (temp1.max() - temp1.min())
-    ghg_temp = temp1.join(ghg)
-    #ghg_temp = pd.concat([temp1, ghg], axis=1)
+    #ghg_temp = temp1.join(ghg)
+    ghg_temp = pd.concat([temp1, ghg], axis=1)
+    ghg_temp.set_index(pd.Series([str(i)
+                                  for i in range(1990, 2011)]), inplace=True)
     temp2 = temp.resample('Q').mean()
 
-    with plt.style.context('Solarize_Light2'):
-        fig = plt.figure()
-        ax1 = fig.add_subplot(2, 2, 1)
-        ghg_temp.plot(ax=ax1, marker='*', linewidth=2, alpha=0.8)
-        ax1.set_xlabel('Years')
-        ax1.set_ylabel('Values')
-        plt.legend(loc='lower right')
-        ax2 = fig.add_subplot(2, 2, 2)
-        ghg_temp.plot.bar(ax=ax2, width=0.6, alpha=0.8)
-        ax2.set_xlabel('Years')
-        ax2.set_ylabel('Values')
-        ax3 = fig.add_subplot(2, 2, 3)
-        temp2.plot.area(ax=ax3, alpha=0.8)
-        ax3.set_xlabel('Quarters')
-        ax3.set_ylabel('Temperature')
-        ax4 = fig.add_subplot(2, 2, 4)
-        temp2.plot.kde(ax=ax4, alpha=0.8)
-        ax4.set_xlabel('Values')
-        ax4.set_ylabel('Values')
-        plt.show()
+    sns.set()
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 2, 1)
+    ghg_temp.plot(ax=ax1)
+    ax1.set_xlabel('Years')
+    ax1.set_ylabel('Values')
+    labels = ['1990', '1992', '1995', '1997',
+             '2000', '2002', '2005', '2007', '2010']
+    ticks = [0, 2.5, 5, 7.5, 10, 12.5, 15, 17.5, 20]
+    ax1.set_xticks(ticks)
+    ax1.set_xticklabels(labels)
+    plt.legend(loc='lower right')
+    ax2 = fig.add_subplot(2, 2, 2)
+    ghg_temp.plot.bar(ax=ax2)
+    ax2.set_xlabel('Years')
+    ax2.set_ylabel('Values')
+    ax3 = fig.add_subplot(2, 2, 3)
+    temp2.plot.area(ax=ax3)
+    ax3.set_xlabel('Quarters')
+    ax3.set_ylabel('Temperature')
+    ax4 = fig.add_subplot(2, 2, 4)
+    temp2.plot.kde(ax=ax4)
+    ax4.set_xlabel('Values')
+    ax4.set_ylabel('Values')
+    plt.show()
     return fig
 
 
