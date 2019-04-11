1e7c0895fd33f7bf474d25b58aecdc1f1eaee51e
redsnowc <nuanyang.44@gmail.com>
2019-04-11 23:05:10 +0800

    update

---
+++ 在线教育网站—视频播放及后台管理实现/simpledu/manage.py
@@ -0,0 +1,3 @@
+from simpledu.app import create_app
+
+app = create_app('development')
---
+++ 在线教育网站—视频播放及后台管理实现/simpledu/simpledu/handlers/admin.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, render_template
+from flask import request, current_app
+from simpledu.models import Course
+from simpledu.decorators import admin_required
+from flask import redirect, url_for, flash
+from simpledu.forms import CourseForm, db
+
+
+admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+@admin.route('/')
+@admin_required
+def index():
+    return render_template('admin/index.html')
+
+@admin.route('/courses')
+@admin_required
+def courses():
+    page = request.args.get('page', default=1, type=int)
+    pagination = Course.query.paginate(
+            page=page,
+            per_page=current_app.config['ADMIN_PER_PAGE'],
+            error_out=False
+        )
+    return render_template('admin/courses.html', pagination=pagination)
+
+@admin.route('/courses/create', methods=['GET', 'POST'])
+@admin_required
+def create_course():
+    form = CourseForm()
+    if form.validate_on_submit():
+        form.create_course()
+        flash('课程创建成功', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/create_course.html', form=form)
+
+@admin.route('/courses/<int:course_id>/edit', methods=['GET', 'POST'])
+@admin_required
+def edit_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    form = CourseForm(obj=course)
+    if form.validate_on_submit():
+        form.update_course(course)
+        flash('课程更新成功', 'success')
+        return redirect(url_for('admin.courses'))
+    return render_template('admin/edit_course.html', form=form, course=course)
+
+@admin.route('/course/<int:course_id>/delete')
+@admin_required
+def delete_course(course_id):
+    course = Course.query.get_or_404(course_id)
+    db.session.delete(course)
+    db.session.commit()
+    flash('课程删除成功', 'success')
+    return redirect(url_for('admin.courses'))
