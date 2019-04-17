d0c06bfb3d94bcdaa0f73e9aa9fc7c728e3a6f8b
redsnowc <nuanyang.44@gmail.com>
2019-04-17 23:50:26 +0800

    update

---
+++ 在线教育网站/在线直播聊天窗口实现/simpledu/simpledu/config.py
@@ -0,0 +1,22 @@
+class BaseConfig(object):
+    SECRET_KEY = 'sfjsdkljfksdjfs;f'
+    INDEX_PER_PAGE = 9
+    ADMIN_PER_PAGE = 15
+
+class DevelopmentConfig(BaseConfig):
+    #FLASK_ENV = 'development'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(BaseConfig):
+    pass
+
+class TestingConfig(BaseConfig):
+    pass
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
