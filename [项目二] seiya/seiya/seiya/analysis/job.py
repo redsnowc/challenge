4779804f3d0ff36ca44d64addc8ea6f19edefadb
redsnowc <nuanyang.44@gmail.com>
2019-04-26 03:10:38 +0800

    更新数据库查询方法

--- [项目二] seiya/seiya/seiya/analysis/job.py
+++ [项目二] seiya/seiya/seiya/analysis/job.py
@@ -3,18 +3,19 @@ from sqlalchemy.sql import func
 
 
 def analysis_top10_city():
-    city_list = session.query(Job.city, func.count(Job.city)).group_by(Job.city).all()
-    city_list = sorted(city_list, key=lambda i: i[1], reverse=True)[:10]
-    data = [{'name': i[0], 'amount': i[1]} for i in city_list]
+    city_list = session.query(
+                Job.city, func.count(Job.city)).group_by(
+                Job.city).order_by(func.count(Job.city).desc()).all()
+    data = [{'name': i[0], 'amount': i[1]} for i in city_list][:10]
     return data
 
+
 def analysis_top10_wage():
-    salary_list = session.query(
-            Job.city, func.avg(Job.salary_upper), 
-            func.avg(Job.salary_lower)).group_by(Job.city).all()
-    slary_list = [
-            {'name' : i[0], 'salary': float((i[1] + i[2]) / 2)} 
-            for i in salary_list]
-    data = sorted(slary_list, key=lambda i: i['salary'], reverse=True)[:10]
+    salary_list = session.query(Job.city, func.avg(
+            (Job.salary_upper+Job.salary_lower)/2)).group_by(
+            Job.city).order_by(func.avg(
+            (Job.salary_upper + Job.salary_lower) / 2).desc()).all()
+    data = [{'name' : i[0], 'salary': float(i[1])}
+            for i in salary_list][:10]
     return data
 
